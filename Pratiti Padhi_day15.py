572cb4496f4d83ad49768c9648b00b148a563596
pratiti2001 <91080827+pratiti2001@users.noreply.github.com>
2021-10-05 21:46:08 +0530

    Add files via upload

---
+++ Pratiti Padhi_day15.py
@@ -0,0 +1,17 @@
+t=int(input())
+students=[]
+a=0
+b=[]
+for i in range(t):
+    n=int(input())
+    students=[int(x) for x in input().split()]
+    for j in range(n):
+        if j+1 not in students:
+            b.append(j+1)
+            a+=1
+    if a==0:
+       print(0)
+    else:
+       print(*b)
+    b=[]
+    a=0
